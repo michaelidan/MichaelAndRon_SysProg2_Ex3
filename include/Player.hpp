d2e140f4fb41c643d2257c57614209b4f8dcc76f
michaelidan <80638926+michaelidan@users.noreply.github.com>
2025-10-31 11:25:31 +0200

    Add Player class with attributes and methods

---
+++ include/Player.hpp
@@ -0,0 +1,19 @@
+// Emails: ronavraham1999@gmail.com_michael9090124@gmail.comlac
+
+#pragma once
+#include <string>
+#include <memory>
+#include "Role.hpp"
+namespace coup {
+class Player {
+public:
+    std::string name; std::unique_ptr<Role> role; int coins{0}; bool alive{true};
+    bool sanctionActive{false}, jamActive{false};
+    bool investedThisTurn{false}, didPeekThisTurn{false}, didJamThisTurn{false};
+    bool forcedCoupFirstAction{false}, bribePendingCoup{false};
+    int actions{0};
+    Player* lastArrestTarget{nullptr};
+    Player(std::string n, std::unique_ptr<Role> r): name(std::move(n)), role(std::move(r)) {}
+    bool isGovernor() const; bool isJudge() const; bool isGeneral() const;
+    bool isBaron() const;    bool isMerchant() const; bool isSpy() const;
+}; }
