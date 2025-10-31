528c8fb9ef3056dd57b5ad9cb1e80469d9277ec5
michaelidan <80638926+michaelidan@users.noreply.github.com>
2025-10-31 11:22:52 +0200

    Add Merchant role class definition

---
+++ include/roles/Merchant.hpp
@@ -0,0 +1,5 @@
+// Emails: ronavraham1999@gmail.com_michael9090124@gmail.comlac
+
+#pragma once
+#include "Role.hpp"
+namespace coup { class Merchant: public Role{ public: std::string name() const override { return "Merchant"; } }; }
